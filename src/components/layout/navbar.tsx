b28d875c5036a3af2d3721c8bffc787e2ca36e4d
Minecraft <minecraft.play.fun.12@gmail.com>
2025-09-20 06:33:27 +0000

    revise and reright code no commit is taking place in github

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -159,7 +159,7 @@ export default function Navbar() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
+      <div className="container flex h-14 items-center px-4 sm:px-6 lg:px-8">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <Image src="/lo.png" alt="Sterling & Landis logo" width={140} height={35} />
